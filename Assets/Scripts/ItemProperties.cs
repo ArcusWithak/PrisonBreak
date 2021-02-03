76c9a996cee77fdf27d9a96f1dc8b457477080fb
ArcusWithak <56251011+ArcusWithak@users.noreply.github.com>
2021-02-03 11:23:50 +0100

    add new item propertie script

---
+++ Assets/Scripts/ItemProperties.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ItemProperties : MonoBehaviour
+{
+    protected string itemName;
+    protected float itemWeight;
+}
